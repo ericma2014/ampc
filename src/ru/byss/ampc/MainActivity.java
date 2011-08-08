1565e64d0ffe6df28f299b8d10bd07a203348ac9
kirrbyss@gmail.com <kirrbyss@gmail.com@f313a3ea-3c74-3c56-bc10-3dc553474532>
2011-08-08 16:01:03 +0000

    Added support for port input and auto-saving settings

--- src/ru/byss/ampc/MainActivity.java
+++ src/ru/byss/ampc/MainActivity.java
@@ -1,25 +1,93 @@
 package ru.byss.ampc;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Spanned;
+import android.text.method.KeyListener;
+import android.text.method.DigitsKeyListener;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+// TODO: create seperate class?
+class NumericRangeKeyListener extends DigitsKeyListener {
+	private static final String TAG = "NumericRangeKeyListener";
+
+	public NumericRangeKeyListener () {
+		this (Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	public NumericRangeKeyListener (int max) {
+		this (0, max);
+	}
+
+	public NumericRangeKeyListener (int min, int max) {
+		super ((min < 0), false);
+		this.min = min;
+		this.max = max;
+	}
+	
+	@Override
+	public CharSequence filter (CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+		boolean ok = true;
+		String new_string = (dest.subSequence (0, dstart).toString () +
+		                    source.subSequence (start, end).toString () +
+		                    dest.subSequence (dend, dest.length ()).toString ()
+		                   );
+		int new_value;
+		if (new_string.length () != 0) {
+			try {
+				new_value = Integer.parseInt (new_string);
+				if ((new_value < min) || (new_value > max)) {
+					ok = false;
+				}
+			}
+			catch (NumberFormatException e) {
+				ok = false;
+			}
+			if (ok) {
+				return null;
+			} else {
+				return dest.subSequence (dstart, dend).toString ();
+			}
+		} else {
+			return null;
+		}
+	}
+	
+	private int min;
+	private int max;
+}
+
 public class MainActivity extends Activity {
 	private static final String TAG = "AMPCMainActivity";
 	
 	private OnClickListener onConnectClickListener = new OnClickListener () {
 		public void onClick (View v) {
-			String host, messageText = "OK";
+			String messageText = "OK";
+			
 			EditText hostEditText = (EditText) findViewById (R.id.host);
-			host = hostEditText.getText ().toString ();
-			if (!control.connect (host)) {
+			String host = hostEditText.getText ().toString ();
+			
+			EditText portEditText = (EditText) findViewById (R.id.port);
+			String port_str = portEditText.getText ().toString ();
+			int port = 0;
+			if (port_str.length () != 0) {
+				port = Integer.parseInt (port_str);
+			}
+			
+			if (!control.connect (host, port)) {
 				messageText = "Failed: " + control.getErrorMessage ();
 			}
+			
 			TextView messageTextView = (TextView) findViewById (R.id.message);
 			messageTextView.setText (messageText);
 		}
@@ -58,6 +126,8 @@ public class MainActivity extends Activity {
 		}
 	};
 	
+	private KeyListener onPortChange = new NumericRangeKeyListener (65535);
+	
 	@Override
 	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate (savedInstanceState);
@@ -70,12 +140,60 @@ public class MainActivity extends Activity {
 		
 		Button connectButton = (Button) findViewById (R.id.connect);
 		connectButton.setOnClickListener (onConnectClickListener);
+
 		Button playButton = (Button) findViewById (R.id.play);
 		playButton.setOnClickListener (onPlayClickListener);
+
 		Button pauseButton = (Button) findViewById (R.id.pause);
 		pauseButton.setOnClickListener (onPauseClickListener);
+
 		Button stopButton = (Button) findViewById (R.id.stop);
 		stopButton.setOnClickListener (onStopClickListener);
+
+		EditText portEditText = (EditText) findViewById (R.id.port);
+		portEditText.setKeyListener (onPortChange);
+		
+		loadSettings ();
+	}
+	
+	@Override
+	public void onPause () {
+		saveSettings ();
+		super.onPause ();
+	}
+	
+	private void loadSettings () {
+		SharedPreferences pref = getPreferences (MODE_PRIVATE);
+		
+		String host = pref.getString ("host", "");
+		EditText hostEditText = (EditText) findViewById (R.id.host);
+		hostEditText.setText (host);
+		
+		int port = pref.getInt ("port", 6600);
+		EditText portEditText = (EditText) findViewById (R.id.port);
+		if (port != 0) {
+			portEditText.setText (Integer.toString (port));
+		} else {
+			portEditText.setText ("");
+		}
+	}
+	
+	private void saveSettings () {
+		Editor pref = getPreferences (MODE_PRIVATE).edit ();
+		
+		EditText hostEditText = (EditText) findViewById (R.id.host);
+		String host = hostEditText.getText ().toString ();
+		pref.putString ("host", host);
+		
+		EditText portEditText = (EditText) findViewById (R.id.port);
+		String port_string = portEditText.getText ().toString ();
+		int port = 0;
+		if (port_string.length () != 0) {
+			port = Integer.parseInt (port_string);
+		}
+		pref.putInt ("port", port);
+		
+		pref.commit ();
 	}
 	
 	private MPDControl control;
